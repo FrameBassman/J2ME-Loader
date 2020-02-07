768873151388747ab5efe5eb5791fe88ac4408ac
Dmitry Romashov <framebassman@gmail.com>
2020-02-07 13:25:30 +0000

    try to broke CI

--- app/src/test/java/ru/playsoftware/j2meloader/MainActivityTest.java
+++ app/src/test/java/ru/playsoftware/j2meloader/MainActivityTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class MainActivityTest {
     @Test
     public void Some() throws Exception {
-//        MainActivity application = new MainActivity();
-//        application.onCreate(new Bundle());
+        MainActivity application = new MainActivity();
+        application.onCreate(new Bundle());
     }
 }
