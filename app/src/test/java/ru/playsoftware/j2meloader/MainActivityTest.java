6d19f3680ddbc9884ade5b8bd6a96235888aabec
Dmitry Romashov <framebassman@gmail.com>
2020-02-07 13:07:19 +0000

    try to add unit test

---
+++ app/src/test/java/ru/playsoftware/j2meloader/MainActivityTest.java
@@ -0,0 +1,11 @@
+package ru.playsoftware.j2meloader;
+
+import org.junit.Test;
+
+public class MainActivityTest {
+    @Test
+    public void Some() throws Exception {
+//        MainActivity application = new MainActivity();
+//        application.onCreate(new Bundle());
+    }
+}
