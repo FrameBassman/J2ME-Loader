fbc3365e63d713fa7943b5ac2bc2fe904e25c315
Dmitry Romashov <framebassman@gmail.com>
2020-02-12 15:03:03 +0000

    spaces to tabs

--- app/src/test/java/ru/playsoftware/j2meloader/MainActivityTest.java
+++ app/src/test/java/ru/playsoftware/j2meloader/MainActivityTest.java
@@ -4,9 +4,9 @@ import android.os.Bundle;
 import org.junit.Test;
 
 public class MainActivityTest {
-    @Test
-    public void Some() throws Exception {
-        MainActivity application = new MainActivity();
-        // application.onCreate(new Bundle());
-    }
+	@Test
+	public void Some() throws Exception {
+		MainActivity application = new MainActivity();
+//		application.onCreate(new Bundle());
+	}
 }
